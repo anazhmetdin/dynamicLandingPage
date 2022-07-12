c832648d57ae691192f5c078f412c9f825efbf0a
anazhmetdin <anazhmetdin@gmail.com>
2022-07-12 05:40:41 +0200

    set port number dinamicaly

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.static('website'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
     console.log(`Server running on port ${port}`);
