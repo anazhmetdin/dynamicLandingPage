6fe2e3cca4a8b36e1dee2b9349c8ab9b643dd7fa
anazhmetdin <anazhmetdin@gmail.com>
2022-07-12 05:35:33 +0200

    use express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('website'));
+
+const port = 3000;
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
